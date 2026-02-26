970cb9aed5869be08a7dd864540795cac14ee302
Evan J. Roden <40574116+evanroden@users.noreply.github.com>
2026-02-26 10:31:36 -0500

    Delete GPlot 2 directory

--- GPlot 2/Gplot Test.R
+++
@@ -1,6 +0,0 @@
-head(Cars93)
-Cars93 %>%
-  filter(MPG.city<36)%>%
-  filter(Cylinders!="rotary")%>%
-  ggplot(aes(x=Price,y=log(MPG.city,base=exp(.1)),col=Cylinders,size=Horsepower))+
-  geom_point(alpha=.6)
